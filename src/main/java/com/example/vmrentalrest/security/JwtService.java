f0f30b183f85b063576c26cd9b66352188477940
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2024-01-23 00:08:40 +0100

    So called minor improvement

--- src/main/java/com/example/vmrentalrest/security/JwtService.java
+++ src/main/java/com/example/vmrentalrest/security/JwtService.java
@@ -76,6 +76,7 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(String token) {
+        token = token.substring(7);
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSigningKey())
