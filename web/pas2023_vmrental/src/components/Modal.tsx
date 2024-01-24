454b29082ece76877196d2e8a97197972bf8f03f
heshsaih <heshsaih@gmail.com>
2024-01-24 19:29:48 +0100

    Minor improvements

--- web/pas2023_vmrental/src/components/Modal.tsx
+++ web/pas2023_vmrental/src/components/Modal.tsx
@@ -3,7 +3,10 @@ import { MouseEventHandler } from "react";
 const ModalComponent = ({ close, Body }: {close: MouseEventHandler, Body: React.ElementType}) => {
     return (
         <div id="modal">
-            <div id="modal-background">
+            <div id="modal-background" onClick={e => {
+                e.stopPropagation();
+                close();
+            }}>
                 <div id="modal-container">
                     <button className="close-button" onClick={close}>X</button>
                     <Body></Body>
--- web/pas2023_vmrental/src/pages/public/DevicesPageComponent.tsx
+++ web/pas2023_vmrental/src/pages/public/DevicesPageComponent.tsx
@@ -1,7 +1,29 @@
+import {useEffect, useState} from "react";
+import {api} from "../../api/api.ts";
+
 const DevicesPageComponent = () => {
+    const [devices, setDevices] = useState([]);
+
+    const fetchDevices = async () => {
+        try {
+            const response = await api.getAllVirtualDevices();
+            setDevices(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDevices();
+    }, []);
+
     return (
-        <h1>This is devices page</h1>
+        <div>
+            { devices.map((x, i) => {
+                return <h1>{JSON.stringify(x)}</h1>
+            }) }
+        </div>
     )
-};
+}
 
 export default DevicesPageComponent;
